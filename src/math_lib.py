837e7865cc27cab4bc182a121cbb073fbe19b7ee
Petr Půček <xpucek03@stud.fit.vutbr.cz>
2021-03-18 21:18:19 +0100

    added frame for math lib

---
+++ src/math_lib.py
@@ -0,0 +1,133 @@
+# ======== Copyright (c) 2021, Vepro-knedlo-zelo s bryndzou, All rights reserved. ============ #
+#
+#  $Purpose:         Calculator - math library
+#  
+#  $University:      Brno University of Technology
+#  $Faculty:         Faculty of Information Technology
+#  $Programme:       Information Technology (Bachelor)
+#  $Course:          Practical Aspects of Software Design (IVS)
+#  $Academic year:   2020/2021 
+#
+#  $Authors:         Dominika Sedilekova    <xsedil00@stud.fit.vutbr.cz> 
+#                    Petr Pucek             <xpucek03@stud.fit.vutbr.cz>   
+#
+# ======== Copyright (c) 2021, Vepro-knedlo-zelo s bryndzou, All rights reserved. ============ #
+
+## 
+#  @file math_lib.py
+# 
+#  @author Dominika Sedilekova
+#  @author Petr Pucek
+#
+#  @brief Implementation of the mathematic functions library
+#
+
+def sum(num1, num2):
+    ##
+    #  @brief sum
+    #  adds two numbers
+    #
+    #  @param num1 first number for addition
+    #  @param num2 second number for addition
+    #
+    #  @return value of addition of two numbers
+
+    ## TO DO ##
+
+    return
+
+def sub(num1, num2):
+    ##
+    #  @brief sub
+    #  subtracts two numbers
+    #
+    #  @param num1 first number for subtraction
+    #  @param num2 second number for subtraction
+    #
+    #  @return value of subtraction of two numbers
+
+    ## TO DO ##
+
+    return
+
+def mult(num1, num2):
+    ##
+    #  @brief mult
+    #  multiples two numbers
+    #
+    #  @param num1 first number for multiplication
+    #  @param num2 second number for multiplication
+    #
+    #  @return value of multiplication of two numbers
+
+    ## TO DO ##
+
+    return
+
+def div(num1, num2):
+    ##
+    #  @brief div
+    #  divides two numbers
+    #
+    #  @param num1 first number for division
+    #  @param num2 second number for division
+    #
+    #  @return value of division of two numbers
+
+    ## TO DO ##
+
+    return
+
+def fact(num):
+    ##
+    #  @brief fact
+    #  calculates factorial of a number
+    #
+    #  @param num number to factore
+    #
+    #  @return value of factorial of a number
+
+    ## TO DO ##
+
+    return
+
+def exp(num, exp):
+    ##
+    #  @brief exp
+    #  calculates factorial of a number
+    #
+    #  @param num base of power
+    #  @param exp exponent of power
+    #
+    #  @return value of exponentiation
+
+    ## TO DO ##
+
+    return
+
+def root(num, deg):
+    ##
+    #  @brief div
+    #  calculates factorial of a number
+    #
+    #  @param num radicand of root
+    #  @param deg degree of root
+    #
+    #  @return value of n-th root
+
+    ## TO DO ##
+
+    return
+
+def abs(num):
+    ##
+    #  @brief abs
+    #  calculates absolute value of a number
+    #
+    #  @param num number
+    #
+    #  @return absolute value of the number
+
+    ## TO DO ##
+
+    return
