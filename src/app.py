d79119963b5ae2aa62a30c7fdcc3d2a01cf54477
Dominika Sedileková <xsedil00@stud.fit.vutbr.cz>
2021-04-19 13:18:47 +0200

    App comments added

--- src/app.py
+++ src/app.py
@@ -1,4 +1,28 @@
+# ======== Copyright (c) 2021, Vepro-knedlo-zelo s bryndzou, All rights reserved. ============ #
+#
+#  $Purpose:         Calculator - math library
+#  
+#  $University:      Brno University of Technology
+#  $Faculty:         Faculty of Information Technology
+#  $Programme:       Information Technology (Bachelor)
+#  $Course:          Practical Aspects of Software Design (IVS)
+#  $Academic year:   2020/2021 
+#
+#  $Authors:         Dominika Sedilekova    <xsedil00@stud.fit.vutbr.cz>  
+#
+# ======== Copyright (c) 2021, Vepro-knedlo-zelo s bryndzou, All rights reserved. ============ #
+
+## 
+#  @file app.py
+# 
+#  @author Dominika Sedilekova
+#
+#  @brief Start of the application
+#
+
 from gui import GUI
 
+## starts gui of application
 gui = GUI()
+## creates app loop (app running)
 gui.root.mainloop()
